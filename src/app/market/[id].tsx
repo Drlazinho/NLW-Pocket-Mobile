fc762ce08de99d42076a7a7d2f144016c389cdf0
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-12-11 23:43:32 -0300

    Camera com Expo

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -4,19 +4,22 @@ import { Coupon } from "@/components/market/coupon";
 import { Cover } from "@/components/market/cover";
 import { Details, PropsDetails } from "@/components/market/details";
 import { api } from "@/services/api";
+import { CameraView, useCameraPermissions } from "expo-camera";
 import { Redirect, router, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
-import { Alert, Text, View } from "react-native";
+import { Alert, Modal, Text, View } from "react-native";
 
 type DataProps = PropsDetails & {
-    cover: string
-}
+  cover: string;
+};
 
 export default function Market() {
   const [data, setData] = useState<DataProps>();
   const [isLoading, setIsLoading] = useState(true);
-  const [coupon, setCoupon] = useState<string | null>(null)
+  const [coupon, setCoupon] = useState<string | null>(null);
+  const [isVisibleCameraModal, setIsVisibleCameraModal] = useState(false);
 
+  const [_, requestPermission] = useCameraPermissions();
   const params = useLocalSearchParams<{ id: string }>();
 
   async function fetchMarket() {
@@ -34,29 +37,53 @@ export default function Market() {
     }
   }
 
+  async function handleOpenCamera() {
+    try {
+      const { granted } = await requestPermission();
+
+      if (!granted) {
+        return Alert.alert("Câmera", "Você precisa habilitar o uso da câmera");
+      }
+
+      setIsVisibleCameraModal(true);
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Câmera", "Não foi possível utilizar a câmera");
+    }
+  }
+
   useEffect(() => {
     fetchMarket();
   }, [params.id]);
 
   if (isLoading) {
-    return <Loading />
+    return <Loading />;
   }
 
   if (!data) {
-    return <Redirect href="/home" />
+    return <Redirect href="/home" />;
   }
 
   return (
     <View style={{ flex: 1 }}>
-        <Cover uri={data.cover}/>
-        <Details data={data}/>
-        {coupon && <Coupon code={'coupon'} />}
+      <Cover uri={data.cover} />
+      <Details data={data} />
+      {coupon && <Coupon code={"coupon"} />}
 
-        <View style={{ padding: 32 }}>
-        <Button>
+      <View style={{ padding: 32 }}>
+        <Button onPress={() => handleOpenCamera()}>
           <Button.Title>Ler QR Code</Button.Title>
         </Button>
       </View>
+
+      <Modal style={{ flex: 1 }} visible={isVisibleCameraModal}>
+        <CameraView style={{ flex: 1 }} facing="back" />
+        <View style={{ position: "absolute", bottom: 32, left: 32, right: 32 }}>
+          <Button onPress={() => setIsVisibleCameraModal(false)}>
+            <Button.Title>Voltar</Button.Title>
+          </Button>
+        </View>
+      </Modal>
     </View>
   );
 }
