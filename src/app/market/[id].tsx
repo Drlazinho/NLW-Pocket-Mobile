fc3d8bed274b7628ec81331f991df6683cdf2982
Lázaro Pimentel <lazbonfim@hotmail.com>
2024-12-11 22:59:10 -0300

    Component Market/Cover

--- src/app/market/[id].tsx
+++ src/app/market/[id].tsx
@@ -1,12 +1,41 @@
-import { useLocalSearchParams } from "expo-router";
-import { Text, View } from "react-native";
+import { Loading } from "@/components/loading";
+import { Cover } from "@/components/market/cover";
+import { api } from "@/services/api";
+import { router, useLocalSearchParams } from "expo-router";
+import { useEffect, useState } from "react";
+import { Alert, Text, View } from "react-native";
 
 export default function Market() {
-    const params = useLocalSearchParams<{ id: string }>()
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useLocalSearchParams<{ id: string }>();
 
-    return (
-        <View style={{ flex: 1}}>
-            <Text>{params.id}</Text>
-        </View>
-    )
+  async function fetchMarket() {
+    try {
+      const { data } = await api.get(`/markets/${params.id}`);
+      setData(data);
+      setIsLoading(false);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível carregar os dados", [
+        {
+          text: "OK",
+          onPress: () => router.back(),
+        },
+      ]);
+    }
+  }
+
+  useEffect(() => {
+    fetchMarket();
+  }, [params.id]);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <View style={{ flex: 1 }}>
+        <Cover uri={data.cover}/>
+    </View>
+  );
 }
